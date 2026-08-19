04cd11c511605e0bea7ada8513d973020cb4eeb2
thePengwinn <38027160+thePengwinn@users.noreply.github.com>
2026-08-19 14:34:28 -0400

    Update data_updater.py
    broke functionalitly into multiple modules. add_update_symbol will make an API call to get daily values for a given symbol, and create a csv if it succesfully pulls data, or update the csv if it is already there.
    remove_symbol will remove a stock csv that is no longer desired in the portfolio. it will not remove sample files.
    cleanup_symbols goes through all the files in the data folder and removes any that do not have enough data to be used (usually this is from either a symbol that doesnt exist or a failed API call)

--- data_updater.py
+++ data_updater.py
@@ -1,36 +1,67 @@
 
 ### Early updater via an API. Use with caution.  DO NOT CHANGE API KEY
+### Kevin will add docstrings and comments
 
 import urllib.request
-import csv
-import io
+import os
 import pandas as pd
 from pathlib import Path
 
 API_KEY = 'OGLS55Q6YJLUVQPP'     #API key, do not change
 
-symbol = 'IBM'
-  
-function_call = 'TIME_SERIES_DAILY'
-url = f'https://www.alphavantage.co/query?function={function_call}&symbol={symbol}&outputsize=full&datatype=csv&apikey={API_KEY}'
+def cleanup_symbols():  #remove files with under 10 rows (that means the file does not have enough data, probably because an API fail)
+    target_dir = Path(__file__).parent / "data"
+    for f in target_dir.iterdir():
+        if f.is_file():
+            df = pd.read_csv(f)
 
-output = Path(__file__).parent / "data" / f"{symbol}.csv"
+            row_count = len(df)
+            #print(f"file {f} - Total rows: {row_count}")
+            if row_count < 10:
+                 if os.path.exists(f):
+                    os.remove(f)
+                 
 
-def updater():
+def add_update_symbol(symbol='AAPL'):
+    function_call = 'TIME_SERIES_DAILY'
+    url = f'https://www.alphavantage.co/query?function={function_call}&symbol={symbol}&datatype=csv&outputsize=full&apikey={API_KEY}'
+    temp = Path(__file__).parent / "data" / f"temp.csv"
+    output = Path(__file__).parent / "data" / f"{symbol}.csv"
     try:
+        if os.path.exists(temp):
+            os.remove(temp)
+
         with urllib.request.urlopen(url) as response:
             data = response.read().decode("utf-8")
-        #print(data)
+        #print([data][0])
 
-        with open(output, "w", encoding="utf-8") as file:
+        with open(temp, "w", encoding="utf-8") as file:
             for line in [data]:
                 cleaned_line = line.rstrip("\n")
-                print(cleaned_line, file=file)
+                if len(cleaned_line) != 0:
+                    print(cleaned_line, file=file)
             
+        df = pd.read_csv(temp)
+        df = df.head(1000)
+        df = df.rename(columns={"timestamp": "Date", "high":"High","low":"Low","open":"Open","close":"Close","volume":"Volume"})
 
+       # print(len(df))
+        if len(df) > 200:
+            df.to_csv(output, index=False)
+
+        if os.path.exists(temp):
+            os.remove(temp)
  
     except:
         print("something went wrong, no file created")
 
+def remove_symbol(symbol):
+    target = Path(__file__).parent / "data" / f"{symbol}.csv"
+    if symbol not in ["aapl_sample", "jpm_sample","msft_sample"]:
+        if os.path.exists(target):
+            os.remove(target)
+
 if __name__ == "__main__":    #allow module to run on its own to test
-    updater()
+    add_update_symbol('AAPL')
+    #cleanup_symbols()
+    #remove_symbol("AAPL")
